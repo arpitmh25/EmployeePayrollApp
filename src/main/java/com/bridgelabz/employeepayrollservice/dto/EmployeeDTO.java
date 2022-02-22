446ecf6ec123eddc29fd5d528c11fe7545ca6856
Arpit <arpitmh23@gmail.com>
2022-02-22 08:26:03 +0530

    Adding validation and pattern to field

--- src/main/java/com/bridgelabz/employeepayrollservice/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/dto/EmployeeDTO.java
@@ -1,13 +1,25 @@
 package com.bridgelabz.employeepayrollservice.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 public class EmployeeDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee firstName is Invalid")
     private String firstName;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee lastName is Invalid")
     private String lastName;
+
+    @NotEmpty(message = "ProfilePic cannot be null")
     private String profilePic;
+
+    @NotEmpty(message = "Department name cannot be null")
     private String department;
-    private Long salary ;
+
+    @Min(value = 500, message = "Salary should be more than 500")
+    private Long salary;
     private LocalDate date;
     private String notes;
 
